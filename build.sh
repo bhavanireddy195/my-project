af40a78e1c262aa760265741e492ea8e705c4ff3
bhavanireddy195 <bhavanidasipalli@gmail.com>
2026-01-01 13:28:35 +0530

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Apply migrations
 python manage.py migrate
